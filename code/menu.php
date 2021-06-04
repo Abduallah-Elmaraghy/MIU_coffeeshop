980d4079c4c8f6c4c6668d4a31cfcdaafd888027
seifeldinahmed731 <83872282+seifeldinahmed731@users.noreply.github.com>
2021-06-04 20:24:49 +0200

    Update menu.php

--- code/menu.php
+++ code/menu.php
@@ -1,54 +1,87 @@
 <?php
 require_once("classes.php");
-$allProducts=Menu::viewProducts();
+session_start();
+  	
+  $allProducts=Product::getAllProducts();
 ?>
 <html>
-	<style>
-		*{
+<link href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'>
+<script src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js'></script>
+<link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+<style>
+	*{
+		/*background-color: #F72323;*/
+	}
+	.main{
+		position: relative;
+		margin-left: 10px;
+		background-color: #eeebdd;
+		padding: 20px;
+		margin-top: 30px;
+		border-radius: 50px;
+		width:450px;
+		padding-top: 200px;
+	
+	}
+		/*img{
+  width: 150px;
+  height: 150.5px;
+  position: absolute;
+  margin-top: -7px;
+  margin-left:-50px;
+}*/
+.mainn{
+	
+	 columns: 50px 3;
+	 column-gap: 3rem;
 
-		}
-		.main{
-			position: relative;
-			margin-left: 10px;
-			background-color: #eeebdd;
-			padding: 20px;
-			margin-top: 30px;
-			border-radius: 50px;
-		}
+	 
+	}
 
-		button:hover{
+	button:hover{
 			background-color: grey;
 		}
+}
 
-	</style>	
-	<body>
-		<h1 style="border-radius: 50px; text-align: center; padding: 15px; margin: 0px; background-color: grey; ">
-			<!--  <img src="miu-logo.jpg">Menu</h1> -->
-			<p style=" background-color:  #F72323; padding: 15px;border-radius: 50px; " img src="miu-logo.jpg">Menu</p>
-			<img style=" margin-top: -90px; margin-right:1000px; width: 100px;height: 50.5px;" src="miu-logo.jpg">
-			</body>
-		<?php	
-		foreach ($allProducts as $product ) {?>
+</style>	
+<body>
+	<h1 style="border-radius: 50px; text-align: center; padding: 15px; margin: 0px; background-color: grey; ">
+	<!--  <img src="miu-logo.jpg">Menu</h1> -->
+	<p style=" background-color:  #F72323; padding: 15px;border-radius: 50px; " img src="miu-logo.jpg">Menu</p>
+	<img style=" margin-top: -90px; margin-right:1000px; width: 100px;height: 50.5px;" src="miu-logo.jpg">
+</body>
+<?php
 
-		<form method="post" action="menu.php?action=add&id=<?php echo$row["id"];?>">
-			<?php 
+	echo"<div class='mainn'>";	
+  	foreach ($allProducts as $product ) {?>
+  		
+  		<form method="post" action="menu.php?action=add&id=<?php echo$row["id"];?>">
+		
+  		<?php 
+  		
+  		
+	  		echo"<div class='main'> ";
+		  		echo"Drink name: ";
+		  		echo $product->name;echo"<br>";
+				echo"Drink price: ";echo $product->StandardPrice;echo"<br>";	
+				echo"Drink description: ";echo $product->Description;	
+				echo"<br><br>";
+				echo "<img style='width: 150px; height: 150.5px; border-radius: 50px;  margin-top: -330px; margin-left:-80px; position:absolute; ' src= '$product->pic' >";
+				
+		  		
 
-											 echo"<div class='main'> ";
-											 echo"Drink name: ";
-											 echo $product->name;echo"<br>";
-											 echo"Drink price: ";echo $product->StandardPrice;echo"<br>";	
-											 echo"Drink description: ";echo $product->Description;	
-											 echo"<br><br>";
-											 echo "<img style='width: 150px; height: 150.5px; border-radius: 50px;  margin-top: -150px; margin-left:-450px; position:absolute; ' src= '$product->pic' >";
+				echo"<button style=' border-radius: 50px; padding: 10px; margin-top: -30px; margin-left:-40px; position:absolute; ' type='submit' name='addtocart'>Add to cart</button>";
+			echo"</div>";
+		
 
-
-											 echo"<button style=' border-radius: 50px; padding: 10px; margin-top: -30px; margin-left:-40px; position:absolute; ' type='submit' name='addtocart'>Add to cart</button>";
-											 echo"</div>";
-											}
-
-			?>
-		</form>
-		</div>
+  	
+}
+  	echo"</div>";
+  ?>
+  </form>
+</div>
 
 </html>  
-
+  
+  
