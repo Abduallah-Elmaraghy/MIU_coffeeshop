b566090035fd2c04373a6c8cdc5f4a8751c7c1b2
sherif elmotayam <sherifelmotayam@gmail.com>
2021-06-04 18:56:37 +0200

    update product view

--- code/productview.php
+++ code/productview.php
@@ -60,7 +60,158 @@ if ($result->num_rows > 0) {
 									<?php  echo "<p>$Description <p>";  ?>
 									<!--E3ml mn dy kaza wa7da l el customizations dy el radio buttons-->
 									<!--dy 70t feha el price m3 el large aw medium 3ala 7asab el size-->
-									<div class="row price">
+									
+                                    <?php
+          echo "<h3>choose your size <h3>";
+
+       $query1="SELECT `id` ,`SizeName` FROM `size` ";
+$result = $conn->query($query1);
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    $id=$row["id"];
+    $name=$row["SizeName"];
+                                       ?>
+                                    
+                                    <div class="size">
+										<label class="radio1">
+											<span>
+												<a><div class="row">
+													<big><b>
+                                                        
+                                            <?php
+                         
+                        
+		echo   "<label for='A' ><input type='radio' class=test value='$id' name='staff1'> $name<br />";
+
+  
+          }
+} else {
+  echo "0 results";
+}
+      
+                                            ?>     
+                                                        </b></big>
+													</div>
+                                                </a>
+											</span> 
+										</label>
+                                    </div>
+                <?php
+      echo "<h3>choose your milk <h3>";
+
+       $query2="SELECT `id` ,`Type` FROM `milk_type` ";
+$result2 = $conn->query($query2);
+if ($result2->num_rows > 0) {
+  // output data of each row
+  while($row2 = $result2->fetch_assoc()) {
+     $id2=$row2["id"];
+    $Typename=$row2["Type"];
+                                       ?>
+                                    
+                                    <div class="milk">
+										<label class="radio2">
+											<span>
+												<a><div class="row">
+													<big><b>
+                                                        
+                                            <?php
+                         
+                        
+		echo "<label for='B' ><input type='radio' value='$id2' name='staff2'> $Typename<br />";
+
+
+  
+          }
+} else {
+  echo "0 results";
+}
+      
+                                            ?>     
+                                                        </b></big>
+													</div>
+                                                </a>
+											</span> 
+										</label>
+                                    </div>
+
+                                    
+                                    
+                                    
+
+                                    
+                                    
+                                    
+                                    
+                                    
+                                    
+                                    
+                                    
+                                    
+                                    
+                                    
+                                        <?php
+echo "<h3>choose your flavors <h3>";
+
+       $query3="SELECT `Id`, `FlavorName` FROM `flavors` ";
+$result3 = $conn->query($query3);
+if ($result3->num_rows > 0) {
+  // output data of each row
+  while($row3 = $result3->fetch_assoc()) {
+
+    $id3=$row3["Id"];
+
+    $FlavorName=$row3["FlavorName"];
+                                       ?>
+                                    
+                                    <div class="flavor">
+										<label class="radio3">
+											<span>
+												<a><div class="row">
+													<big><b>
+                                                        
+                                            <?php
+                         
+                        
+		echo "<label for='B' ><input type='radio' value='$id3' name='staff3'> $FlavorName<br />";
+
+
+  
+          }
+} else {
+  echo "0 results";
+}
+      
+                                            ?>     
+                                                        </b></big>
+													</div>
+                                                </a>
+											</span> 
+										</label>
+                                    </div>
+
+                                    
+                                    
+                                    
+                                    
+                                    
+                                    
+                                    
+                                    
+                                    
+                                    
+                                    
+                                    
+                                    
+                                    
+                                    
+                                    
+                                    
+                                    
+                                    
+                                    
+                           
+                                    <div class="row price">
 										<label class="radio">
 											<input type="radio" name="size1" value="small" checked>
 											<span>
